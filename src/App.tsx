550b7dec4a82b65ab0d14dbe9a6d560b511102ed
fmasouza <fabmacsouza@gmail.com>
2020-10-19 23:43:24 -0300

    Add component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import './App.css';
+import Header from './core/components/Header';
+import './app.scss';
 
 function App() {
   return (
-    <h1>App</h1>
+      <div>
+        <Header />
+      </div>
   );
 }
 
---
+++ src/core/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Header = () => {
+    return (
+        <header className="main-header bg-primary">
+            <span className="logo-text">Bootcamp DevSuperior</span>
+        </header>
+    );
+}
+
+export default Header;
